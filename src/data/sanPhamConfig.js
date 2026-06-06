b40578fb2b8ac651e7ce112d559fb97f961c7d44
dangdoanminhkha <minhkha05876@gmail.com>
2026-06-06 12:20:44 +0700

    Thêm loại tính

--- src/data/sanPhamConfig.js
+++ src/data/sanPhamConfig.js
@@ -1,7 +1,7 @@
 // File: src/data/sanPhamConfig.js
 
 export const LOAI_TINH_OPTIONS = [
-    "Răng", "Răng (không đếm)", "Bán hàm", "Hàm", "Khác"
+    "Răng", "Răng (không đếm)", "Bán hàm", "Hàm trên dưới", "Hàm trên", "Hàm dưới", "Khác"
 ];
 
 export const LOAI_SAN_PHAM_OPTIONS = [

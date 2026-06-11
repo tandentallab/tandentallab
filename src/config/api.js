6b5cd2c6f78dee7fb24481669ebb2fb98e4e89fd
hoangquan204 <quanpogba888@gmail.com>
2026-06-11 22:42:53 +0700

    Thêm search theo tên và lọc chức vụ cho bảng lương 2

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// export const API_URL = process.env.REACT_APP_API_URL;
-
-export const API_URL = 'http://localhost:8080/api/';
+export const API_URL = process.env.REACT_APP_API_URL;
 
 export const api = axios.create({
     baseURL: API_URL,
